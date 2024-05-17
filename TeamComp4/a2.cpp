99c13b29716c39654baca3f485bb52789a6e5f46
Leonard Weininger <leonard.weininger@rwth-aachen.de>
2024-05-17 16:27:33 +0200

    Fixed a2.cpp

--- TeamComp4/a2.cpp
+++ TeamComp4/a2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -11,10 +12,27 @@ int main() {
 		std::cin >> sticks[i];
 	}
 
+	std::sort(sticks.begin(), sticks.end());
+
+	while (!sticks.empty() && sticks.back() > n) {
+		sticks.pop_back();
+		--k;
+	}
+
+	std::vector<std::vector<bool>> win(1 << k, std::vector<bool>(n + 1, false));
+
 	for (int len = 0; len <= n; ++len) {
-		std::cout << len << " WIN\n";
+		for (int mask = 0; mask < (1 << k); ++mask) {
+			for (int i = 0; i < k; ++i) {
+				if ((mask & (1 << i)) && sticks[i] <= len) {
+					if (!win[mask ^ (1 << i)][len - sticks[i]]) {
+						win[mask][len] = true;
+					}
+				}
+			}
+		}
+		std::cout << len << ' ' << (win[(1 << k) - 1][len] ? "WIN" : "LOSE") << '\n';
 	}
 
 	return 0;
 }
-
